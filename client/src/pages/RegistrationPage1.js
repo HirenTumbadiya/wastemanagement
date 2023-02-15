729f9fd66843296ae910c1fc88bd200e0223ec64
hiren <tumbadiyahiren@gmail.com>
2023-02-15 23:00:57 +0530

    pages updated

--- client/src/pages/RegistrationPage1.js
+++ client/src/pages/RegistrationPage1.js
@@ -1,63 +1,15 @@
 import { useCallback } from "react";
-import React, { Component } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./RegistrationPage1.css";
-import { Link, withRouter } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { registerUser } from "../actions/authActions";
-import classnames from "classnames";
 
-// const navigate = useNavigate();
+const RegistrationPage1 = () => {
+  const navigate = useNavigate();
 
-class RegistrationPage1 extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      password2: "",
-      errors: {}
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors
-      });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
-    };
-console.log(newUser);
-this.props.registerUser(newUser, this.props.history); 
-  };
-
-// const RegistrationPage1 = () => {
-  // const navigate = useNavigate();
-
-  // const onSignInTextClick = useCallback(() => {
-    // navigate("/");
-  // }, [navigate]);
-
-  render() {
-    const { errors } = this.state;
+  const onSignInTextClick = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
 
   return (
-    <>
     <div className="registration-page1">
       <div className="create-account">Create Account</div>
       <img
@@ -71,32 +23,10 @@ this.props.registerUser(newUser, this.props.history);
         alt=""
         src="../digitalscreenwithenvironmentday-1@2x.png"
       />
-      <form className="sign1" method="post" onSubmit={this.onSubmit}>
-          <input
-          onChange={this.onChange}
-          value={this.state.name}
-          error={errors.name}
-          id="name"
-          type="text"
-          className={classnames("rectangle-input", {
-            invalid: errors.name
-          })} />
-        <input className="sign-child1"
-        onChange={this.onChange}
-        value={this.state.email}
-        error={errors.email}
-        id="email"
-        type="email"
-        />
-        <input 
-        onChange={this.onChange}
-        value={this.state.password}
-        error={errors.password}
-        id="password"
-        type="password"
-        className={classnames("sign-child2", {
-          invalid: errors.email
-        })} />
+      <form className="sign1" method="post">
+        <input className="rectangle-input" type="text" />
+        <input className="sign-child1" type="text"  />
+        <input className="sign-child2" type="password"  />
         <div className="name">Name</div>
         <div className="email1">Email</div>
         <div className="password1">Password</div>
@@ -104,29 +34,15 @@ this.props.registerUser(newUser, this.props.history);
         <div className="i-agree-to">
           I agree to the Terms and Privacy Policy.
         </div>
-        <button className="sign-child4" type="submit" />
-        <button className="sign-child5" type="submit"/>
+        <button className="sign-child4" />
+        <button className="sign-child5" />
         <div className="sign-up">Sign Up</div>
-        <div className="sign-in1">
-          <Link to="/login">Sign In</Link>
+        <div className="sign-in1" onClick={onSignInTextClick}>
+          Sign In
         </div>
       </form>
     </div>
-    </>
   );
 };
-};
-RegistrationPage1.propTypes = {
-  registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
-};
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-});
-// export default RegistrationPage1;
-export default connect(
-  mapStateToProps,
-  { registerUser }
-)((RegistrationPage1));
+
+export default RegistrationPage1;
